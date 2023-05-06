7db3b959308ecac870c82c0110e83cf0f714fcc7
Raze2023 <132787311+Raze2023@users.noreply.github.com>
2023-05-06 18:45:38 -0300

    Update config.js

--- Configs/config.js
+++ Configs/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: '',
-    prefix: '',
-    devs: '',
+    token: 'MTEwNDUyMTQxODI0ODU1MjQ5OA.Ge2J4v.lDifnlVMNPwhN7mat6nGDBU5bX0lpGH-JTjP6M',
+    prefix: '$',
+    devs: '<#1078737020463104070>',
     embedcolor: '',
     database: '',
     FACT_API: '',
